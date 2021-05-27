a2522c0594c9487760d53b9430d966db2bafbd68
Joseph Smith <N0797675@ntu.ac.uk>
2021-05-27 10:48:42 +0100

    Added functionality for user to choose whether to print all stores, available stores or favourite stores

--- Tgtg Notifier.py
+++ Tgtg Notifier.py
@@ -1,26 +1,56 @@
 from tgtg import TgtgClient
 import pgeocode
-import pycountry
+#import pycountry
 
 def fetchData():
     email = input("Email address: ")
     password = input("Password: ")
     postcode = input("Postcode: ")
-    radius = input("How many miles radius should be scanned? ")
 
-    geocode = pgeocode.Nominatim('gb')
-    geoinfo = geocode.query_postal_code(postcode)
+    geocode = pgeocode.Nominatim('gb') #Create an instance of pgeocode for Great Britain
+    geoinfo = geocode.query_postal_code(postcode) #Query postcode in Great Britain to retrieve latitude and longitude data
     latitude = geoinfo['latitude']
     longitude = geoinfo['longitude']
 
-    print("Connecting to",email,"with password:","Just kidding ;)\n")
+    print("Connecting to",email,"with password:","Just kidding ;)")
     client = TgtgClient(email=email, password=password)
 
-    displayData(client.get_items(
-        favorites_only=False,
-        latitude = latitude,
-        longitude = longitude,
-        radius = 10))
+    command = input("Use command 'help' to print available commands\n\n").lower()
+    running = True
+    while running == True:
+        if command == "help":
+            print("all - Fetch a list of all stores\navailable - Fetch a list of available stores\nfav - Fetch a list of your favourite stores\nquit - Exit the program\n")
+            command = input()
+        elif command == "all":
+            radius = input("How many miles radius should be scanned? ")
+            displayAllData(client.get_items( #Pass list created by client.get_items 
+                favorites_only=False,
+                latitude = latitude,
+                longitude = longitude,
+                radius = radius))
+            command = input()
+        elif command == "available":
+            radius = input("How many miles radius should be scanned? ")
+            displayAvailableData(client.get_items( #Pass list created by client.get_items 
+                favorites_only=False,
+                latitude = latitude,
+                longitude = longitude,
+                radius = radius))
+            command = input()
+        elif command == "fav":
+            favCommand = input("all - Fetch a list of all favourite stores\navailable - Fetch a list of available favourite stores\n\n")
+            if favCommand == "all":
+                displayAllData(client.get_items())
+            elif favCommand == "available":
+                displayAvailableData(client.get_items())
+            else:
+                print("Unrecognised command")
+            command = input()
+        elif command == "quit":
+            running = False
+        else:
+            print("Unrecognised command")
+            command = input()
 
 def parsePrice(price, x):
     if x['item']['price']['code'] == "GBP":
@@ -31,19 +61,37 @@ def parsePrice(price, x):
         price = "$" + price
     return price
     
-def displayData(items):
+def displayAvailableData(items):
+    data = []
+    for x in items:
+        if x['items_available'] != 0:
+            displayName = x['display_name']
+            price = str(x['item']['price']['minor_units'])
+            decimals = len(str(x['item']['price']['minor_units'])) - x['item']['price']['decimals'] 
+            price = price[:decimals] + "." + price[decimals:] + " " + x['item']['price']['code']
+            price = parsePrice(price, x) 
+            data.append(displayName+" - "+price)
+    if len(data) == 0:
+        print("No stores currently available\n")
+    else:
+        print("\nAvailable stores: \n",data,"\n")
+
+def displayAllData(items):
+    data = []
     for x in items:
         displayName = x['display_name']
         price = str(x['item']['price']['minor_units'])
         decimals = len(str(x['item']['price']['minor_units'])) - x['item']['price']['decimals'] 
         price = price[:decimals] + "." + price[decimals:] + " " + x['item']['price']['code']
-        price = parsePrice(price, x) 
-        availablity = "available"
+        price = parsePrice(price, x)
+        availability = "available"
         if x['items_available'] == 0:
             availability = "sold out"
-        if availability == "sold out":
-            print (displayName,"-",price)
-
+        data.append(displayName+" - "+price+" - "+availability)
+    if len(data) == 0:
+        print("No stores found\n")
+    else:
+        print("\n",data,"\n")
 fetchData()
 
 

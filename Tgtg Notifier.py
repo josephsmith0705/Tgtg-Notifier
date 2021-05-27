c2ac1c68830892b3f1587059d50d9429073eba2d
Joseph Smith <N0797675@ntu.ac.uk>
2021-05-27 09:53:24 +0100

    Added functionality to scan all stores within a given radius of the provided postcode, rather than only the user's favourite stores

--- Tgtg Notifier.py
+++ Tgtg Notifier.py
@@ -1,27 +1,50 @@
 from tgtg import TgtgClient
+import pgeocode
+import pycountry
 
-email = input("Email address: ")
-password = input("Password: ")
+def fetchData():
+    email = input("Email address: ")
+    password = input("Password: ")
+    postcode = input("Postcode: ")
+    radius = input("How many miles radius should be scanned? ")
 
-print("Connecting to",email,"with password:","Just kidding ;)\n")
-client = TgtgClient(email=email, password=password)
+    geocode = pgeocode.Nominatim('gb')
+    geoinfo = geocode.query_postal_code(postcode)
+    latitude = geoinfo['latitude']
+    longitude = geoinfo['longitude']
 
-items = client.get_items()
+    print("Connecting to",email,"with password:","Just kidding ;)\n")
+    client = TgtgClient(email=email, password=password)
 
-for x in items:
-    displayName = x['display_name']
-    price = str(x['item']['price']['minor_units'])
-    decimals = x['item']['price']['decimals']
-    decimals = len(price) - decimals
-    price = price[:decimals] + "." + price[decimals:] + " " + x['item']['price']['code']
+    displayData(client.get_items(
+        favorites_only=False,
+        latitude = latitude,
+        longitude = longitude,
+        radius = 10))
+
+def parsePrice(price, x):
     if x['item']['price']['code'] == "GBP":
         price = "£" + price
     elif x['item']['price']['code'] == "EUR":
         price = "€" + price
     elif x['item']['price']['code'] == "USD":
         price = "$" + price
-    availablity = "available"
-    if x['items_available'] == 0:
-        availability = "sold out"
-    print (displayName,"-",price,"-",availability)
+    return price
+    
+def displayData(items):
+    for x in items:
+        displayName = x['display_name']
+        price = str(x['item']['price']['minor_units'])
+        decimals = len(str(x['item']['price']['minor_units'])) - x['item']['price']['decimals'] 
+        price = price[:decimals] + "." + price[decimals:] + " " + x['item']['price']['code']
+        price = parsePrice(price, x) 
+        availablity = "available"
+        if x['items_available'] == 0:
+            availability = "sold out"
+        if availability == "sold out":
+            print (displayName,"-",price)
+
+fetchData()
+
+
     
